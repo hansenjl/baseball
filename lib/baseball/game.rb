777d2b68287b249c2f7d1b3e4e2eb9f96c1deef5
hansenjl <jenn.leigh.hansen@gmail.com>
2019-03-12 20:46:43 -0700

    Setup for study group

--- lib/baseball/game.rb
+++ lib/baseball/game.rb
@@ -1,37 +1,10 @@
 class Game
-  attr_accessor :team, :home, :score, :result, :date
+  attr_accessor :team, :home, :score, :result, :date, :url
   @@all = []
 
-  ## option 1
-  # def initialize(team,score,result,date, home)
-  #   @team = team
-  #   @score = score
-  #   @result = result
-  #   @home = home
-  #   @date = date
-
-  #   @@all << self
-  # end
-
-
-
-   ## option 2
-#   def initialize(attributes)
-#     attributes.each do |key,value|
-#       self.send("#{key.to_s}=",value)
-#     end
-
-#     # @team = "Royals"
-#     # self.team= "Royals"
-#     # self.send("team=", "Royals")
-#   end
-
-
-  ## option 3
-
-  # def initialize
-  #   @@all << self
-  # end
+  def initialize
+    @@all << self
+  end
 
   def self.all
     @@all
--- lib/baseball/scraper.rb
+++ lib/baseball/scraper.rb
@@ -12,43 +12,14 @@ class Scraper
     games = section.css("li")  #selecting all of the li tags that hold games
 
     games.each do |game|
-
-      ##option 1 - passing in attributes as arguments
-
-      # home_and_team = game.css("div.game-info").text.split
-      # team = home_and_team[1]
-      # home = home_and_team[0]
-      # date =  game.css("div.game-date").text
-      # score = game.css("div.score").text
-      # result = game.css("div.game-result").text
-
-      # Game.new(team,score,result,date, home)
-
-
-
-      ##option 2 - the SEND method
-
-      # attributes = {
-      #   team: home_and_team[1],
-      #   home: home_and_team[0],
-      #   date:  game.css("div.game-date").text,
-      #   score: game.css("div.score").text,
-      #   result: game.css("div.game-result").text
-      # }
-      # Game.new(attributes)
-
-
-
-      ##option 3 - The Clean Model Method
-      ##using the writer/setter method from the scraper
-
-        # game_object = Game.new
-        # home_and_team = game.css("div.game-info").text.split
-        # game_object.team = home_and_team[1]
-        # game_object.home = home_and_team[0]
-        # game_object.date =  game.css("div.game-date").text
-        # game_object.score = game.css("div.score").text
-        # game_object.result = game.css("div.game-result").text
+        game_object = Game.new
+        home_and_team = game.css("div.game-info").text.split
+        game_object.team = home_and_team[1]
+        game_object.home = home_and_team[0]
+        game_object.date =  game.css("div.game-date").text
+        game_object.score = game.css("div.score").text
+        game_object.result = game.css("div.game-result").text
+        game_object.url = "http://www.espn.com" + game.css("a")[0]['href'] if game.css("a")[0]['href']
     end
 
   end
